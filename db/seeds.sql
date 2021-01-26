ff6285e4bbfbb6efd9ae457ee7ba24d55f497c2c
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-25 19:08:38 -0500

    Created a seeds sql file. It inserts 6 new rows with unique burger names.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Seeds
+
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('The Original Burger');
+INSERT INTO burgers (name) VALUES ('Whiskey King Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (name) VALUES ('The Ghost Burger');
+INSERT INTO burgers (name) VALUES ('The Varsity Burger');
