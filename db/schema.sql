2a5601c4721fd00bcc0ae723a4dcdff7010875ce
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-26 21:22:20 -0500

    Updated the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
--- Schema
-
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-USE burgers_db;
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
+	devouredly BOOLEAN DEFAULT 0,
 	PRIMARY KEY (id)
 );
